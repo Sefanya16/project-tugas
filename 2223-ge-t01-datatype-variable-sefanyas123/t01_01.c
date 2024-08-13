f5eca8c17442649ccaa7f5601df45dabec212c92
Sefanya16 <153581058+Sefanya16@users.noreply.github.com>
2024-08-13 15:33:37 +0700

    Add files via upload

---
+++ 2223-ge-t01-datatype-variable-sefanyas123/t01_01.c
@@ -0,0 +1,31 @@
+// 12S22026 - Ruben Sinipar
+// 12s22051 - Sefanya Sinaga
+
+#include <stdio.h>
+
+int main(int _argc, char **_argv)
+{
+  
+ int sasuke;
+ scanf("%d", &sasuke);
+
+ char aw;
+ scanf(" %s", &aw);
+
+ int bol;
+ scanf("%d", &bol);
+
+ unsigned int neg;
+ scanf("%u", &neg);
+
+ float angka;
+ scanf("%f", &angka);
+
+ printf("%d\n", sasuke);
+ printf("%s\n", aw);
+ printf("%d\n", bol);
+ printf("%u\n", neg);
+ printf("%.2f\n", angka);
+
+  return 0;
+}
